4101e19b0845523efcb5a5e8ac875c76ef00f38e
Jyoshna Malluru <jyoshna1595@gmail.com>
2024-08-16 17:33:23 +0530

    add changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 import { Route,BrowserRouter as Router,Routes } from 'react-router-dom'
 import LandingPage from './pages/landing.jsx'
+import Authentication from './pages/authentication.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 
 function App() {
   return (
    <div className='App'>
       <Router>
-        <Routes>
-          {/* <Route path="/home" element= /> */}
-          <Route path="/" element={<LandingPage />}/>
-          <Route path="/auth" element={<Authentication />}/>
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            {/* <Route path="/home" element= /> */}
+            <Route path="/" element={<LandingPage />}/>
+            <Route path="/auth" element={<Authentication />}/>
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
