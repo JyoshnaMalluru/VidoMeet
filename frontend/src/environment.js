bc946dc26b9b41d559d480f7a85e6a4f35106a7e
Jyoshna Malluru <jyoshna1595@gmail.com>
2024-12-24 15:52:37 +0530

    Deploying Project

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://vidomeetbackend.onrender.com" :
 
     "http://localhost:8000"
 
