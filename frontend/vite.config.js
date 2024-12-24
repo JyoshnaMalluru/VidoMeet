82c95467cf0fb5fe20771c5b9891f74087608b97
Jyoshna Malluru <jyoshna1595@gmail.com>
2024-12-24 17:22:50 +0530

    add changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures the built files go to the 'dist' folder
+  },
 })
 
 // npm run dev -- --port 8000  frontend
